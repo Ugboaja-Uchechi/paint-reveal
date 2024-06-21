d6b8ab9f05fbe301d260fa4a6b1af2b409e1e328
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2024-06-21 19:38:40 +0100

    Drawing circles on mouse move

--- app/component/scene.js
+++ app/component/scene.js
@@ -6,7 +6,7 @@ export default function Scene() {
   const { dimension } = useWindow();
   const canvas = useRef();
 
-  useEffect( () => {
+  useEffect(() => {
     dimension.width > 0 && init();
   }, [dimension])
 
@@ -16,12 +16,28 @@ export default function Scene() {
     ctx.fillRect(0, 0, dimension.width, dimension.height);
     //the destination-out global composite operation is used on the canvas so that every time we draw additional shapes on the canvas, it will erase the black rectangle used as a background.
     ctx.globalCompositeOperation = "destination-out";
-  }
+  };
+
+  const manageMouseMove = (e) => {
+    const { clientX, clientY } = e;
+
+    draw(clientX, clientY, 50)
+  };
+
+  const draw = (x, y, radius) => {
+    const ctx = canvas.current.getContext("2d");
+
+    ctx.beginPath();
+
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+
+    ctx.fill();
+  };
 
   return (
     <div className='relative w-full h-full'>
-      {dimension.width == 0 && <div className='absolute w-full h-full bg-black'/>}
-      <canvas ref={canvas} height={dimension.height} width={dimension.width}/>
+      {dimension.width == 0 && <div className='absolute w-full h-full bg-black' />}
+      <canvas ref={canvas} height={dimension.height} width={dimension.width} onMouseMove={manageMouseMove} />
     </div>
   )
 }
